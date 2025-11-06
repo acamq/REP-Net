ec605f7ea3f2aca1063c16d5bfcc357123c7e529
acamq <al.charmquark@gmail.com>
2025-11-06 12:16:30 -0700

    Add data

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 if __name__ == '__main__':
     dry_run = True
 
-    #ecl_96.run(dry_run=dry_run)
+    ecl_96.run(dry_run=dry_run)
     ettm1_96.run(dry_run=dry_run)
     etth1_96.run(dry_run=dry_run)
